51b1ec06f062fe57f1f5cfae391fd3f1a5f1a0ad
SMR <Smrazavi1991@hotmail.com>
2023-05-16 17:10:36 +0330

    Add loclhost to alowed_host setting

--- src/config/settings.py
+++ src/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-g#t0h6k(-us(r(l6tqvern#7vki8g8&@%oq&f!tf^bqm2w(=u+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
