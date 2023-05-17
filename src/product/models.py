bc45d4b1d478199d60c4deb2994dfe26f1c5ced1
SMR <Smrazavi1991@hotmail.com>
2023-05-17 18:09:01 +0330

    update models fields. nearly completed

--- src/product/models.py
+++ src/product/models.py
@@ -1,56 +1,55 @@
 from django.db import models
 from core.models import BaseModel
-
+from core.fields import JDateTimeField
+from user.models import User
 
 # Create your models here.
-class Category(BaseModel):
-    pass
-    # name
-    # discount
-    # is_active
-    # info_item
-    # sub
-    # discount_activate_at
-    # discount_deactivate_at
-    # discount_is_active
-
-
 class InformationItem(BaseModel):
-    pass
-    # name
-
-
-class Product(BaseModel):
-    pass
-    # name
-    # #category
-    # brand
-    # count
-    # discount
-    # product_info
-    # price
-    # discount_activate_at
-    # discount_deactivate_at
-    # discount_is_active
-
-
-class ProductComment(BaseModel):
-    pass
-    # title
-    # description
-    # product
-    # owner
-    # reply
-    # is_approved
-
-
-class ProductImage(BaseModel):
-    pass
-    # image
-    # product
+    name = models.CharField(max_length=100)
 
 
 class Discount(BaseModel):
-    pass
-    # type
-    # amount
+    types = [
+        ('%', 'percentage'),
+        ('$', 'non-percentage')
+    ]
+    type = models.CharField(choices=types, max_length=1)
+    amount = models.IntegerField()
+    # must have validator
+
+class Category(BaseModel):
+    name = models.CharField(max_length=100)
+    discount = models.ForeignKey(Discount, on_delete=models.SET_NULL, null=True)
+    is_active = models.BooleanField('Category Is Active', default=True)
+    info_item = models.ManyToManyField(InformationItem, null=True)
+    sub = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    discount_activate_at = JDateTimeField('Activate at')
+    discount_deactivate_at = JDateTimeField('Deactivate at')
+    discount_is_active = models.BooleanField('Active discount', default=False)
+
+
+class Product(BaseModel):
+    name = models.CharField(max_length=100)
+    category = models.ManyToManyField(Category)
+    brand = models.CharField(max_length=100)
+    count = models.IntegerField()
+    discount = models.ForeignKey(Discount, on_delete=models.SET_NULL, null=True)
+    product_info = models.JSONField()
+    price = models.IntegerField()
+    discount_activate_at = JDateTimeField('Activate at')
+    discount_deactivate_at = JDateTimeField('Deactivate at')
+    discount_is_active = models.BooleanField('Active discount', default=False)
+
+
+class ProductComment(BaseModel):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default="Anonymous")
+    reply = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    is_approved = models.BooleanField('Comment Approved', default=False)
+
+
+class ProductImage(BaseModel):
+    image = models.ImageField(upload_to='media/product_images')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
--- src/user/models.py
+++ src/user/models.py
@@ -1,22 +1,36 @@
 from django.contrib.auth.models import AbstractUser
 from core.models import BaseModel
 from django.db import models
+from core.fields import JDateField
 
 
 # Create your models here.
-class User(AbstractUser):
-    pass
-    #phone
-    #birthday
-    #gender
-    #profile_pic
-    #phone_verified
-    #address
-    #is_staff = none
-
 class Address(BaseModel):
-    pass
-    #province
-    #city
-    #address
-    #postal code
+    province = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    address = models.CharField(max_length=2000)
+    postal_code = models.CharField(max_length=10)
+
+
+class User(AbstractUser):
+    genders = [
+        ('m', 'male'),
+        ('f', 'female')
+    ]
+
+    email = models.EmailField('email address', unique=True)
+    phone = models.CharField(max_length=11, unique=True)
+    # must have validator
+    birthday = JDateField('Birth Date', null=True)
+    gender = models.CharField(choices=genders, max_length=1)
+    profile_pic = models.ImageField(null=True, upload_to='media/user_profile_pic', blank=True)
+    phone_verified = models.BooleanField('Phone Verified', default=False)
+    address = models.ManyToManyField(Address, null=True)
+    is_staff = None
+
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
