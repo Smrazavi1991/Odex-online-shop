ef756c579c892d17b543e475cb4cb072bf479853
SMR <Smrazavi1991@hotmail.com>
2023-05-17 15:41:07 +0330

    Add base models and its field names (just names)

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    #is_deleted
+    #last_change
+    #create_date
+    #delete_datetime
