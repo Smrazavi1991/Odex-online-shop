3b78dbc83cbd38c61143643ff189df90b3078128
SMR <Smrazavi1991@hotmail.com>
2023-05-17 15:42:46 +0330

    Add user models and its field names (just names)

--- src/user/models.py
+++ src/user/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser):
+    pass
+    #phone
+    #birthday
+    #gender
+    #profile_pic
+    #phone_verified
+    #address
+    #is_staff = none
