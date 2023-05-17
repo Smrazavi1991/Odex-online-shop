1886f0c32aa85bb59c183aa447683be65da17c92
SMR <Smrazavi1991@hotmail.com>
2023-05-17 16:31:52 +0330

    complete base model fields

--- src/core/models.py
+++ src/core/models.py
@@ -6,7 +6,7 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
 
-    #is_deleted
-    #last_change
-    #create_date
-    #delete_datetime
+    is_deleted = models.BooleanField('Is Deleted', default=False, null=False, blank=False)
+    last_change = models.DateTimeField(auto_now=True)
+    create_date = models.DateTimeField(auto_created=True)
+    delete_datetime = models.DateTimeField(default=None, null=True, blank=True)
