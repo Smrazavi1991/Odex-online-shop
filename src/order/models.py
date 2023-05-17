6fd9533357586c66a5ad34edbd2ff93e335512a4
SMR <Smrazavi1991@hotmail.com>
2023-05-17 16:01:12 +0330

    add order models field names (just names)

--- src/order/models.py
+++ src/order/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from core.models import BaseModel
+
 
 # Create your models here.
+class Cart(BaseModel):
+    pass
+    # customer
+    # item
+    # shipping_price
+
+
+class Order(BaseModel):
+    pass
+    # cart
+    # status
+
+
+class DiscountCoupon(BaseModel):
+    pass
+    # code
+    # discount
+    # owner
