422f1e5af3e1b25651baebdf49fd5f2a78f86a68
SMR <Smrazavi1991@hotmail.com>
2023-05-18 20:27:13 +0330

    replace custom field that created with (jdatetime) library in this model with django_jalali modules, and create a function to fill delete_datetime.

--- src/core/models.py
+++ src/core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from .fields import JDateTimeField
+from django_jalali.db import models as jmodels
+import jdatetime
+import datetime
 
 
 # Create your models here.
@@ -8,6 +10,10 @@ class BaseModel(models.Model):
         abstract = True
 
     is_deleted = models.BooleanField('Is Deleted', default=False, null=False, blank=False)
-    last_change = JDateTimeField('Last Change', auto_now=True)
-    create_date = JDateTimeField('Create Date', auto_created=True)
-    delete_datetime = JDateTimeField('Delete Datetime', default=None, null=True, blank=True)
+    last_change = jmodels.jDateTimeField('Last Change', auto_now=True, editable=False)
+    create_date = jmodels.jDateTimeField('Create Date', auto_now_add=True, editable=False)
+    delete_datetime = jmodels.jDateTimeField('Delete Datetime', default=None, null=True, blank=True, editable=False)
+
+    def create_delete_datetime(self):
+        if self.is_deleted is True:
+            self.delete_datetime = jdatetime.datetime.fromgregorian(datetime=datetime.datetime)
