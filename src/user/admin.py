529599fceba06ca0547f09f99d4646aac0a0f1a3
SMR <Smrazavi1991@hotmail.com>
2023-05-18 20:21:29 +0330

    add django_jalali widget to show persian calendar in admin panel

--- src/user/admin.py
+++ src/user/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import *
+from django_jalali.admin.filters import JDateFieldListFilter
+
+
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_filter = (
+        ('date', JDateFieldListFilter),
+    )
+
+
 admin.site.register(User)
 admin.site.register(Address)
