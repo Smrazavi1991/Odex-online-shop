d358cf8b381ace6be93d7c73773d6be9e98acda7
SMR <Smrazavi1991@hotmail.com>
2023-05-18 20:29:22 +0330

    replace custom field that created with (jdatetime) library in this model with django_jalali modules, and change discount model, add __str__ method to category and informationitem models, and add blank=True for comments.

--- src/product/models.py
+++ src/product/models.py
@@ -1,29 +1,22 @@
-from django.core.exceptions import ValidationError
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from core.models import BaseModel
-from core.fields import JDateTimeField
+from django_jalali.db import models as jmodels
 from user.models import User
 
-
-def max_discount_percentage(value, discount_type):
-    if discount_type == '%' and value > 80:
-        raise ValidationError(f"maximum percentage of discount is set to 80")
-
-
 # Create your models here.
+
+
 class InformationItem(BaseModel):
     name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Discount(BaseModel):
-    types = [
-        ('%', 'percentage'),
-        ('$', 'non-percentage')
-    ]
-
-    discount_type = models.CharField(choices=types, max_length=1)
-    amount = models.IntegerField(validators=[max_discount_percentage])
+    amount_of_percentage_discount = models.IntegerField(validators=[MaxValueValidator(80)], null=True, blank=True)
+    amount_of_non_percentage_discount = models.IntegerField(null=True, blank=True)
 
 
 class Category(BaseModel):
@@ -32,10 +25,12 @@ class Category(BaseModel):
     is_active = models.BooleanField('Category Is Active', default=True)
     info_item = models.ManyToManyField(InformationItem)
     sub = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
-    discount_activate_at = JDateTimeField('Activate at', null=True, blank=True)
-    discount_deactivate_at = JDateTimeField('Deactivate at', null=True, blank=True)
+    discount_activate_at = jmodels.jDateTimeField('Discount Activate at', null=True, blank=True)
+    discount_deactivate_at = jmodels.jDateTimeField('Discount Deactivate at', null=True, blank=True)
     discount_is_active = models.BooleanField('Active discount', default=False)
 
+    def __str__(self):
+        return self.name
 
 class Product(BaseModel):
     name = models.CharField(max_length=100)
@@ -45,8 +40,8 @@ class Product(BaseModel):
     discount = models.ForeignKey(Discount, on_delete=models.SET_NULL, null=True, blank=True)
     product_info = models.JSONField()
     price = models.IntegerField(validators=[MinValueValidator(0)])
-    discount_activate_at = JDateTimeField('Activate at', null=True, blank=True)
-    discount_deactivate_at = JDateTimeField('Deactivate at', null=True, blank=True)
+    discount_activate_at = jmodels.jDateTimeField('Discount Activate at', null=True, blank=True)
+    discount_deactivate_at = jmodels.jDateTimeField('Discount Deactivate at', null=True, blank=True)
     discount_is_active = models.BooleanField('Active discount', default=False)
 
 
@@ -55,7 +50,7 @@ class ProductComment(BaseModel):
     description = models.CharField(max_length=1000)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    reply = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    reply = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
     is_approved = models.BooleanField('Comment Approved', default=False)
 
 
