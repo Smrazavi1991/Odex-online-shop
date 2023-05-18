ed7f9afe73e4226adea50b03b8f2bd7d5e49e22a
SMR <Smrazavi1991@hotmail.com>
2023-05-18 20:32:30 +0330

    add django_jalali app to settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_jalali",
     "core",
     "order",
     "product",
@@ -112,6 +113,8 @@ LANGUAGE_CODE = "en-us"
 TIME_ZONE = "Asia/Tehran"
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
