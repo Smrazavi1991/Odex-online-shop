c23b5b925aaafa59bc4183c2ef6c1d650531d58b
SMR <Smrazavi1991@hotmail.com>
2023-05-20 21:20:02 +0330

    delete redundant import (MinValueValidator), add verbose name to some fields, make info_item field of category model nullable and blank, override save function for product and category model to manage deactivate discount time when activate a discount without mention deactivate time.

--- src/product/models.py
+++ src/product/models.py
@@ -1,33 +1,42 @@
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MaxValueValidator
 from django.db import models
 from core.models import BaseModel
 from django_jalali.db import models as jmodels
 from user.models import User
+import jdatetime
 
 # Create your models here.
 
 
 class InformationItem(BaseModel):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField('Name', max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Discount(BaseModel):
-    amount_of_percentage_discount = models.PositiveIntegerField(validators=[MaxValueValidator(80)], null=True, blank=True)
-    amount_of_non_percentage_discount = models.PositiveIntegerField(null=True, blank=True)
+    amount_of_percentage_discount = models.PositiveIntegerField('Amount Of Percentage Discount', validators=[MaxValueValidator(80)], null=True, blank=True, unique=True)
+    amount_of_non_percentage_discount = models.PositiveIntegerField('Amount Of NON-Percentage Discount', null=True, blank=True, unique=True)
 
 
 class Category(BaseModel):
     name = models.CharField(max_length=100, unique=True)
     discount = models.ForeignKey(Discount, on_delete=models.SET_NULL, null=True, blank=True)
     is_active = models.BooleanField('Category Is Active', default=True)
-    info_item = models.ManyToManyField(InformationItem)
+    info_item = models.ManyToManyField(InformationItem, null=True, blank=True)
     sub = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
     discount_activate_at = jmodels.jDateTimeField('Discount Activate at', null=True, blank=True)
     discount_deactivate_at = jmodels.jDateTimeField('Discount Deactivate at', null=True, blank=True)
     discount_is_active = models.BooleanField('Active discount', default=False)
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        if self.discount_activate_at is not None:
+            if self.discount_deactivate_at is None:
+                self.discount_deactivate_at = jdatetime.datetime.fromgregorian(year=2111, month=3, day=21)
+        return super().save()
 
     def __str__(self):
         return self.name
@@ -44,6 +53,13 @@ class Product(BaseModel):
     discount_deactivate_at = jmodels.jDateTimeField('Discount Deactivate at', null=True, blank=True)
     discount_is_active = models.BooleanField('Active discount', default=False)
 
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        if self.discount_activate_at is not None:
+            if self.discount_deactivate_at is None:
+                self.discount_deactivate_at = jdatetime.datetime.fromgregorian(year=2111, month=3, day=21)
+        return super().save()
 
 class ProductComment(BaseModel):
     title = models.CharField(max_length=100)
