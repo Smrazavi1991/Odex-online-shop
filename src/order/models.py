091038168d433d4b6c3637cc293ee9efa7dfd279
SMR <Smrazavi1991@hotmail.com>
2023-05-20 21:14:26 +0330

    add three jalali datetime field for discount management and change order and cart relation field to 'OneToOneField' and add verbose name for discound field in DiscountCoupon class

--- src/order/models.py
+++ src/order/models.py
@@ -1,8 +1,12 @@
+import datetime
+
 from django.core.validators import MinValueValidator
 from django.db import models
 from core.models import BaseModel
 from user.models import User
 from product.models import Discount
+from django_jalali.db import models as jmodels
+import jdatetime
 
 
 # Create your models here.
@@ -19,11 +23,14 @@ class Order(BaseModel):
         ('s', 'sent'),
         ('d', 'delivered')
     ]
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.OneToOneField(Cart, on_delete=models.CASCADE)
     status = models.CharField('Status', choices=statuses, max_length=1)
 
 
 class DiscountCoupon(BaseModel):
-    code = models.CharField(max_length=10)
+    code = models.CharField('Discount Code', max_length=10)
     discount = models.ForeignKey(Discount, on_delete=models.CASCADE)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    discount_activate_at = jmodels.jDateTimeField('Discount Activate at')
+    discount_deactivate_at = jmodels.jDateTimeField('Discount Deactivate at', default=jdatetime.datetime.fromgregorian(year=2111, month=3, day=20, hour=20, minute=30).replace(tzinfo=datetime.timezone.utc))
+    discount_is_active = models.BooleanField('Active discount', default=True)
