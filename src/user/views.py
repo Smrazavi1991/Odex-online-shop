baeff7e0e39b6df360468ca658de888f808a22d0
SMR <Smrazavi1991@hotmail.com>
2023-05-22 21:22:15 +0330

    Add an initial view for register/login functionality

--- src/user/views.py
+++ src/user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from core.views import BasicViewMixin
 
-# Create your views here.
+
+class RegisterOrLogin(View, BasicViewMixin):
+    def get(self, request):
+        self.template_name = "user/register-login.html"
+        return super().get(request)
