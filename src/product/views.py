f46e9a71720e8183591ef1fb7e8549834c35342a
SMR <Smrazavi1991@hotmail.com>
2023-05-22 13:43:37 +0330

    Get all Category object and send it to front-side and change 'Category' view name to 'CategoryProducts' to avoid conflict with Category model

--- src/product/views.py
+++ src/product/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import ListView
+from .models import Category
 
 
 # Create your views here.
 class Home(View):
     def get(self, request):
+        categories = Category.objects.all()
+        return render(request, "landing_page_base.html", {"categories": categories})
 
-        return render(request, "landing_page_base.html")
 
-class Category(ListView):
-    def get(self, request):
+class CategoryProducts(ListView):
+    def get(self, request, pk):
+        # model =
         return render(request, "landing_page_base.html")
 
