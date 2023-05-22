0b384b68700ac265409781d80242c801e2df0193
SMR <Smrazavi1991@hotmail.com>
2023-05-22 12:59:45 +0330

    Add Category and Home views

--- src/product/views.py
+++ src/product/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView
 
 
 # Create your views here.
-def Example(request):
-    return render(request, "landing_page_base.html")
+class Home(View):
+    def get(self, request):
+
+        return render(request, "landing_page_base.html")
+
+class Category(ListView):
+    def get(self, request):
+        return render(request, "landing_page_base.html")
+
