434b5b974d117ba7c8533035f34caac546169c94
SMR <Smrazavi1991@hotmail.com>
2023-05-22 21:20:30 +0330

    add a mixin that contain some common contents that be used in nearly all views

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,17 @@
+from django.db.models import Q
 from django.shortcuts import render
+from django.views import View
+from product.models import Category
+
 
 # Create your views here.
+class BasicViewMixin:
+
+    criterion1 = Q(is_deleted=False)
+    criterion2 = Q(is_active=True)
+    categories = Category.objects.filter(criterion1 & criterion2)
+    queryset = {"categories": categories}
+    template_name = "landing_page_base.html"
+
+    def get(self, request):
+        return render(request, self.template_name, self.queryset)
