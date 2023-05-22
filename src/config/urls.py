1b1ca341462dcc158ffe187d7955f6a2ff338dd5
SMR <Smrazavi1991@hotmail.com>
2023-05-22 13:42:42 +0330

    temporarily remove include from imports (I will add it again when I need it) and change 'Category' view name to 'CategoryProducts' to avoid conflict with Category model

--- src/config/urls.py
+++ src/config/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf.urls.static import static
 from product.views import *
 from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("Category/<int:pk>/",Category, name="Category page"),
-    path("", Home, name="Home page")
+    path("Category/<int:pk>/", CategoryProducts.as_view(), name="Category page"),
+    path("", Home.as_view(), name="Home page")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
