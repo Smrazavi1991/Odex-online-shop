c4c1d4c48254b223e5929e8525e9365c83017b8b
SMR <Smrazavi1991@hotmail.com>
2023-05-23 23:22:57 +0330

    Add search field to admin panels

--- src/order/admin.py
+++ src/order/admin.py
@@ -5,6 +5,7 @@ from .models import *
 # Register your models here.
 class CartAdmin(admin.ModelAdmin):
     list_display = ('id', 'item', 'shipping_price', 'customer_id', 'last_change', 'is_deleted')
+    search_fields = ("item", )
 
 
 class OrderAdmin(admin.ModelAdmin):
@@ -13,6 +14,7 @@ class OrderAdmin(admin.ModelAdmin):
 
 class DiscountCouponAdmin(admin.ModelAdmin):
     list_display = ('id', 'code', 'discount_id', 'owner_id', 'discount_is_active', 'last_change', 'is_deleted')
+    search_fields = ("code",)
 
 
 admin.site.register(Cart, CartAdmin)
