fd453f348f6da3eb8c3aa1c04ee076f0101dce6a
SMR <Smrazavi1991@hotmail.com>
2023-05-23 20:51:50 +0330

    Add list_display attr to show customize infos in admin panel

--- src/order/admin.py
+++ src/order/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import *
 
+
 # Register your models here.
-admin.site.register(Cart)
-admin.site.register(Order)
-admin.site.register(DiscountCoupon)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'item', 'shipping_price', 'customer_id', 'last_change', 'is_deleted')
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cart_id', 'status', 'last_change', 'is_deleted')
+
+
+class DiscountCouponAdmin(admin.ModelAdmin):
+    list_display = ('id', 'code', 'discount_id', 'owner_id', 'discount_is_active', 'last_change', 'is_deleted')
+
+
+admin.site.register(Cart, CartAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(DiscountCoupon, DiscountCouponAdmin)
