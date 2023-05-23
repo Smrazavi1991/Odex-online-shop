3ce36ffabb19c320921bae5ecbfcb4b2ff82cd92
SMR <Smrazavi1991@hotmail.com>
2023-05-23 20:30:12 +0330

    Add list_display attr to show customize objects infos in admin panel

--- src/user/admin.py
+++ src/user/admin.py
@@ -10,5 +10,14 @@ class UserAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(User)
-admin.site.register(Address)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name', 'gender', 'email', 'phone', 'birthday',
+                    'phone_verified', 'date_joined', 'is_active', 'is_superuser', 'last_login')
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('id', 'province', 'city', 'address', 'postal_code', 'last_change', 'is_deleted')
+
+
+admin.site.register(User, UsersAdmin)
+admin.site.register(Address, AddressAdmin)
