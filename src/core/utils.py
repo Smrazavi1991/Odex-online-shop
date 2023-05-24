64ab3922692abe3311e5a3de52a9ae0c34a6240b
SMR <Smrazavi1991@hotmail.com>
2023-05-24 21:28:00 +0330

    Add two functions: a function to create a jwt token and a function to find a user role from token

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,4 +1,7 @@
 from product.models import Category
+from django.contrib.auth.models import Group
+import jwt
+from config.secrets import SECRET_KEY
 
 
 def categories_cascade_deactivation_check():
@@ -16,3 +19,18 @@ def categories_cascade_deactivation_check():
             if category2.is_deleted is True or category2.is_active is False:
                 category1.is_active = False
                 category1.save()
+
+
+def identify_user_role(user_id: int):
+    list_of_roles = []
+    user_roles = Group.objects.filter(user__id=user_id)
+    if not user_roles:
+        list_of_roles = None
+    for user_role in user_roles:
+        list_of_roles.append(user_role.name)
+    return list_of_roles
+
+
+def create_jwt_token(payload: dict):
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    return {'token': token.decode('utf-8')}
