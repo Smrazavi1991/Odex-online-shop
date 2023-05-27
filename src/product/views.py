55d649074c09f19b2ad92e8e4b5078b5b6b6e760
SMR <Smrazavi1991@hotmail.com>
2023-05-27 15:37:02 +0330

    Change Home class ingeritance from simple views.View to generic.ListView

--- src/product/views.py
+++ src/product/views.py
@@ -1,52 +1,44 @@
 from django.db.models import Q
 from django.shortcuts import render
-from core.views import BasicViewMixin
+from core.views import BasicViewMixin, ProductsViewMixin
 from django.views.generic import ListView
 from .models import ProductImage, Product, Category
 from django.views import View
 
 
 # Create your views here.
-class Home(View, BasicViewMixin):
-    list_of_product_image = []
-    price_after_discount = []
+class Home(ListView, BasicViewMixin, ProductsViewMixin):
     condition1 = Q(discount_is_active=True)
     condition2 = Q(discount=True)
-    discount_products = Product.objects.filter(condition1 & condition2)
+    queryset = Product.objects.filter(condition1 & condition2)
+    template_name = "product/home_page.html"
 
-    for product in discount_products:
-        temp_dict = {}
-        product_image = ProductImage.objects.filter(product_id=product.id).first()
-        list_of_product_image.append(product_image)
-
-        if product.discount.amount_of_percentage_discount:
-            _ = product.price * (1 - product.discount.amount_of_percentage_discount / 100)
-        else:
-            _ = product.price - product.discount.amount_of_non_percentage_discount
-
-        categories = Category.objects.filter(product__id=product.id)
-        for category in categories:
-            if category.discount_is_active:
-                if category.discount.amount_of_percentage_discount:
-                    _ = product.price_after_discount * (1 - category.discount.amount_of_percentage_discount / 100)
-                else:
-                    _ = product.price_after_discount - product.discount.amount_of_non_percentage_discount
-        temp_dict.setdefault("id", product.id)
-        temp_dict.setdefault("price", _)
-        price_after_discount.append(temp_dict)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = self.category['categories']
+        context["product_pictures"] = ProductsViewMixin.get_pics_from_a_product_queryset(queryset=self.queryset)
+        context["discounted_price"] = ProductsViewMixin.get_discount_price_from_a_product_queryset(queryset=self.queryset)
+        print(context)
+        return context
 
 
-    def get(self, request):
-        self.template_name = "product/home_page.html"
-        self.queryset.setdefault("discount_products", self.discount_products)
-        self.queryset.setdefault("product_pictures", self.list_of_product_image)
-        self.queryset.setdefault("discounted_price", self.price_after_discount)
-        return super().get(request)
-
-
-class CategoryProducts(ListView):
+class CategoryProducts(ListView, BasicViewMixin, ProductsViewMixin):
     def get_queryset(self):
         return Product.objects.filter(category=self.kwargs['pk'])
-    template_name = "landing_page_base.html"
+    template_name = "product/list_of_product_of_a_category.html"
 
+    def get_category(self):
+        return Category.objects.get(pk=self.kwargs['pk'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = BasicViewMixin.queryset['categories']
+        for product in self.get_queryset():
+            if product in self.discount_products:
+                product
+        context['product_pictures'] = self.list_of_product_image
+        context['discounted_price'] = self.price_after_discount
+        context['category'] = self.get_category()
+        print(context)
+        return context
 
