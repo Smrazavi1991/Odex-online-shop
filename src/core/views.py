f8dfb77e68e788f98b1f307abc85c1492a1d2d60
SMR <Smrazavi1991@hotmail.com>
2023-05-27 15:35:07 +0330

    Add two static method to BasicViewMixin class to make them easy to use.

--- src/core/views.py
+++ src/core/views.py
@@ -1,7 +1,5 @@
 from django.db.models import Q
-from django.shortcuts import render
-from django.views import View
-from product.models import Category
+from product.models import Category, ProductImage
 
 
 # Create your views here.
@@ -10,8 +8,39 @@ class BasicViewMixin:
     criterion1 = Q(is_deleted=False)
     criterion2 = Q(is_active=True)
     categories = Category.objects.filter(criterion1 & criterion2)
-    queryset = {"categories": categories}
+    category = {"categories": categories}
     template_name = "landing_page_base.html"
 
-    def get(self, request):
-        return render(request, self.template_name, self.queryset)
+
+class ProductsViewMixin:
+
+    @staticmethod
+    def get_pics_from_a_product_queryset(queryset: list):
+        list_of_product_image = []
+        for product in queryset:
+            product_image = ProductImage.objects.filter(product_id=product.id).first()
+            list_of_product_image.append(product_image)
+        return list_of_product_image
+
+    @staticmethod
+    def get_discount_price_from_a_product_queryset(queryset: list):
+        price_after_discount = []
+        for product in queryset:
+            temp_dict = {}
+
+            if product.discount.amount_of_percentage_discount:
+                _ = product.price * (1 - product.discount.amount_of_percentage_discount / 100)
+            else:
+                _ = product.price - product.discount.amount_of_non_percentage_discount
+
+            categories = Category.objects.filter(product__id=product.id)
+            for category in categories:
+                if category.discount_is_active:
+                    if category.discount.amount_of_percentage_discount:
+                        _ = product.price_after_discount * (1 - category.discount.amount_of_percentage_discount / 100)
+                    else:
+                        _ = product.price_after_discount - product.discount.amount_of_non_percentage_discount
+            temp_dict.setdefault("id", product.id)
+            temp_dict.setdefault("price", _)
+            price_after_discount.append(temp_dict)
+        return price_after_discount
