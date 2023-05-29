0214374280b7c5d00ab70aeec018f52925790476
SMR <Smrazavi1991@hotmail.com>
2023-05-29 16:24:32 +0330

    Add a view for generate a Token and a view for test it

--- src/user/api/v1/views.py
+++ src/user/api/v1/views.py
@@ -1,10 +1,37 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from .serializers import ObtainTokenSerializer
+from user.models import User
 
 
-class ObtainToken(APIView):
-    def post(self, request):
-        serializer_ = OtpSerializer(request.data)
-        serializer_.is_valid(raise_exeption= True)
-        serializer_.save()
-        return Response(serializer_.data)
+class ObtainTokenView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = ObtainTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        username = serializer.validated_data.get('username')
+        password = serializer.validated_data.get('password')
+
+        user = User.objects.filter(username=username).first()
+        if user is None or not user.check_password(password):
+            return Response({'message': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Generate the JWT token
+        jwt_token = JWTAuthentication.create_jwt(user)
+
+        return Response({'token': jwt_token})
+
+
+class Profile(APIView):
+    permission_classes = [IsAuthenticated,]
+    def get(self, request):
+        content = {"message": "yes we can"}
+        return Response(content)
+
