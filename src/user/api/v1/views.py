e9a918f0974ed0bde6a2d7a0da25290e6902a1fb
SMR <Smrazavi1991@hotmail.com>
2023-05-30 11:20:51 +0330

    Add user_role identifier to obtain token view to login a staff into django admin panel when login to website,too, Create a new view for users that login with otp code.

--- src/user/api/v1/views.py
+++ src/user/api/v1/views.py
@@ -1,11 +1,15 @@
+from django.contrib.auth import login
+from django.db.models import Q
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from user.authentication import JWTAuthentication
+import redis
 
-from .serializers import ObtainTokenSerializer
+from .serializers import ObtainTokenSerializer, ObtainTokenOTPSerializer
 from user.models import User
+from core.utils import identify_user_role
 
 
 class ObtainTokenView(APIView):
@@ -22,6 +26,39 @@ class ObtainTokenView(APIView):
         user = User.objects.filter(username=username).first()
         if user is None or not user.check_password(password):
             return Response({'message': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+        elif identify_user_role(user.id):
+            login(request, user)
+
+        # Generate the JWT token
+        jwt_token = JWTAuthentication.create_jwt(user)
+
+        response = Response({'token': jwt_token})
+        response.set_cookie("token", jwt_token)
+
+        return response
+
+
+class ObtaintTokenOTPView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = ObtainTokenOTPSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        verification_code = serializer.validated_data.get('verification_code')
+
+        r = redis.Redis(host='localhost', port=6379, db=0)
+        user_identifier = request.COOKIE.get('user_email_or_phone', None)
+        storedcode = r.get(user_identifier).decode()
+        if verification_code == storedcode:
+            condition1 = Q(email=user_identifier)
+            condition2 = Q(phone=user_identifier)
+            user = User.objects.filter(condition1 | condition2).first()
+            if user is None:
+                return Response({'message': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+            elif identify_user_role(user.id):
+                login(request, user)
 
         # Generate the JWT token
         jwt_token = JWTAuthentication.create_jwt(user)
