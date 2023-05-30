99599bed288d92657b963260d2be8af48b20e264
SMR <Smrazavi1991@hotmail.com>
2023-05-30 18:15:32 +0330

    Add view file for order app APIs and define a view for add product to cart. still needs work

---
+++ src/order/api/v1/views.py
@@ -0,0 +1,35 @@
+import datetime
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+from .serializers import AddToCartViewSerializer
+
+
+class AddToCartView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = AddToCartViewSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        pk = serializer.validated_data.get('pk')
+        name = serializer.validated_data.get('name')
+        price = serializer.validated_data.get('price')
+        image = serializer.validated_data.get('image')
+        count = serializer.validated_data.get('count')
+        temp_dict = {'pk': pk, 'name': name, 'price': price, 'image': image, 'count': count}
+
+        cart = request.COOKIES.get('cart', None)
+        if not cart:
+            cart = []
+        cart.append(temp_dict)
+
+        response = Response({'cart': 'ok'})
+        expires = datetime.datetime.now() + datetime.timedelta(weeks=999)
+        expires_string = expires.strftime("%a, %d-%b-%Y %H:%M:%S")
+        response.set_cookie("cart", cart, expires=expires_string)
+
+        return response
