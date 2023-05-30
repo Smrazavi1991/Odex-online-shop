8e9953ddbdd3ed3f147ed11092101101c229a0a2
SMR <Smrazavi1991@hotmail.com>
2023-05-30 10:16:21 +0330

    Add and change path to test endpoints

--- src/user/api/v1/urls.py
+++ src/user/api/v1/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
 from user.api.v1.views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path("obtaintoken/", ObtainToken.as_view(), name="Obtain Token"),
+    path('token/', ObtainTokenView.as_view(), name='token_obtain_pair'),
+    # path("obtaintoken/", ObtainToken.as_view(), name="Obtain Token"),
     # path("register/", Register.as_view(), name="Register"),
     # path("verification/", Verification.as_view(), name="Verification"),
     # path("login/", Login.as_view(), name="Login"),
-    # path("profile/", Profile.as_view(), name="Profile"),
+    path("profile/", Profile.as_view(), name="Profile"),
     # path("category/<int:pk>/", CategoryProducts.as_view(), name="Category-page"),
     # path("product/<int:pk>/", ProductDetails.as_view(), name="Product-page"),
     # path("api/v1/", include('user.api.v1.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
