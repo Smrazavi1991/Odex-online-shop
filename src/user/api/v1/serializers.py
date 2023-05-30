f95df9391b6c56432674d8ade2f6e66b134c8eb3
SMR <Smrazavi1991@hotmail.com>
2023-05-30 11:15:45 +0330

    Add a new serializer to handle DRF login with OTP

--- src/user/api/v1/serializers.py
+++ src/user/api/v1/serializers.py
@@ -4,3 +4,7 @@ from rest_framework import serializers
 class ObtainTokenSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
+
+
+class ObtainTokenOTPSerializer(serializers.Serializer):
+    verification_code = serializers.CharField(max_length=6)
