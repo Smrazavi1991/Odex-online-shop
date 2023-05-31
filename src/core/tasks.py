2815ae725bccc6a807fc85060c128f97cb929c60
SMR <Smrazavi1991@hotmail.com>
2023-05-31 15:16:19 +0330

    Try to correct send SMS functionality that is unsuccessful. still needs work

--- src/core/tasks.py
+++ src/core/tasks.py
@@ -3,19 +3,26 @@ from kavenegar import *
 import random
 import redis
 from celery import shared_task
+import requests
+API_KEY = '7A51505630436F50615247437A64326E3564524439413262354E6A6246344E39412B573530764863327A6F3D'
 
 
 @shared_task
 def send_opt_sms(phone: str, expiry_time: int):
+    code = random.randint(100000, 999999)
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    r.setex(phone, expiry_time, str(code))
     try:
-        code = random.randint(100000, 999999)
-        api = KavenegarAPI('7A51505630436F50615247437A64326E3564524439413262354E6A6246344E39412B573530764863327A6F3D')
-        r = redis.Redis(host='localhost', port=6379, db=0)
-        r.setex(phone, expiry_time, str(code))
-        params = {'sender': '1000596446', 'receptor': phone, 'message': f'{code}.وب سرویس پیام کوتاه کاوه نگار'}
-        response = api.sms_send(params)
-        print(response)
-    except (APIException, HTTPException) as e:
+        api = KavenegarAPI(API_KEY)
+        params = {
+        'receptor': phone,
+        'template': 'verify',
+        'token': code,
+        }
+        response = api.verify_lookup(params)
+    except APIException as e:
+        print(e)
+    except HTTPException as e:
         print(e)
 
 
