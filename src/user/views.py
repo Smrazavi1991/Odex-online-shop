5d60b55716b123ac9267a02464d5684dc0bae32f
SMR <Smrazavi1991@hotmail.com>
2023-05-31 15:15:38 +0330

    Try to show error in login page if invalid data entered but unsuccessful. so just provide categories in reloaded register page front, from view in case of wrong data entered.

--- src/user/views.py
+++ src/user/views.py
@@ -21,7 +21,7 @@ class Register(View, BasicViewMixin):
         if form.is_valid():
             form.save()
             return redirect('Login')
-        return render(request, 'user/register.html', {'form': form})
+        return render(request, 'user/register.html', {"categories": self.categories, 'form': form})
 
 
 class Verification(View, BasicViewMixin):
@@ -38,31 +38,34 @@ class Login(View, BasicViewMixin):
     def post(self, request):
         form = SendOTPForm(request.POST)
         if form.is_valid():
+            user = None
             if re.match(r'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}', form.cleaned_data['mail_phone']):
-                if User.objects.get(email=form.cleaned_data['mail_phone']):
+                try:
+                    user = User.objects.get(email=form.cleaned_data['mail_phone'])
+                except User.DoesNotExist:
+                    pass
+                if user:
                     send_opt_email.delay(form.cleaned_data['mail_phone'], 300)
                     response = redirect('Verification')
                     expiry_minutes = 5
-                else:
-                    raise Exception('invalid email')
 
             elif re.match(r'^(09)\d{9}$', form.cleaned_data['mail_phone']):
-                if User.objects.get(phone=form.cleaned_data['mail_phone']):
+                try:
+                    user = User.objects.get(phone=form.cleaned_data['mail_phone'])
+                except User.DoesNotExist:
+                    pass
+                if user:
                     send_opt_sms.delay(form.cleaned_data['mail_phone'], 60)
                     response = redirect('Verification')
                     expiry_minutes = 1
-                else:
-                    raise Exception('invalid phone')
 
-            else:
-                return render(request, 'user/login.html', {'form': form})
+            if user:
+                expires = datetime.datetime.now() + datetime.timedelta(minutes=expiry_minutes)
+                expires_string = expires.strftime("%a, %d-%b-%Y %H:%M:%S")
+                response.set_cookie("user_email_or_phone", form.cleaned_data['mail_phone'], expires=expires_string)
+                return response
 
-            expires = datetime.datetime.now() + datetime.timedelta(minutes=expiry_minutes)
-            expires_string = expires.strftime("%a, %d-%b-%Y %H:%M:%S")
-            response.set_cookie("user_email_or_phone", form.cleaned_data['mail_phone'], expires=expires_string)
-            return response
-
-        return render(request, 'user/login.html', {'form': form})
+        return render(request, 'user/login.html', {"categories": self.categories, 'form': form})
 
 
 class Profile(View):
