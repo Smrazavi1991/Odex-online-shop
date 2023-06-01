f892d771a58f211968b3ba50e49a22985788c29d
SMR <Smrazavi1991@hotmail.com>
2023-06-01 23:25:24 +0330

    Rewrite user API serializers due to change that occurred in the login method.

--- src/user/api/v1/serializers.py
+++ src/user/api/v1/serializers.py
@@ -1,10 +1,8 @@
 from rest_framework import serializers
+from user.models import User
 
 
-class ObtainTokenSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
-
-
-class ObtainTokenOTPSerializer(serializers.Serializer):
-    verification_code = serializers.CharField(max_length=6)
+class UserInformationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'phone', 'birthday', 'gender', 'profile_pic', 'is_active']
