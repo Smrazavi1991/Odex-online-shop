ce39ffaa17f6949bd02be5886d673d1cea1eaa43
SMR <Smrazavi1991@hotmail.com>
2023-06-03 13:08:16 +0330

    Fix a bug that sometimes cause error in add or remove item in cart, and fix total price calculator of cart items functionality.

--- src/core/views.py
+++ src/core/views.py
@@ -15,7 +15,7 @@ class BasicViewMixin:
     @staticmethod
     def get_user_cart(request, total: False):
         cart = request.COOKIES.get("cart", None)
-        if cart is None:
+        if not cart:
             return None
         else:
             product_list = []
@@ -25,7 +25,7 @@ class BasicViewMixin:
             for product in range(len(temp)):
                 products = eval(temp[product])
                 products['name'] = products['name'].decode('utf-8')
-                total_price += int(products['price'])
+                total_price += int(products['price']) * products['count']
                 if len(temp_name_count) == 0:
                     temp_name_count = {int(products['pk']): products['count']}
                     product_list.append(products)
