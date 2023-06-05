3bbe80049af80cbb8319451e114d75fb6c6bf29b
SMR <Smrazavi1991@hotmail.com>
2023-06-05 23:58:56 +0330

    Add a simple view for Cart page that just show a raw page

--- src/order/views.py
+++ src/order/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from core.views import BasicViewMixin
+
 
 # Create your views here.
+class Cart(View, BasicViewMixin):
+    def get(self, request):
+        return render(request, "order/cart.html", {"categories": self.categories})
