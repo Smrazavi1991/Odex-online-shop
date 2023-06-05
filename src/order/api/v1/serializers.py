fdbc23dbed007ee55c31cf02ee03ac9bb86cad0d
SMR <Smrazavi1991@hotmail.com>
2023-06-05 23:42:58 +0330

    Correct serializer according to request data format and add a new serializer for CalculateTotal APIview

--- src/order/api/v1/serializers.py
+++ src/order/api/v1/serializers.py
@@ -15,7 +15,11 @@ class RemoveFromCartViewSerializer(serializers.Serializer):
 class UpdateCartSerializer(serializers.Serializer):
     pk = serializers.CharField()
     name = serializers.CharField(max_length=100)
-    price = serializers.CharField()
-    discounted_price = serializers.CharField()
+    price = serializers.IntegerField()
     count = serializers.IntegerField()
     image = serializers.ImageField()
+
+
+class CalculateTotalSerializer(serializers.Serializer):
+    total_price = serializers.IntegerField()
+    total_count = serializers.IntegerField()
