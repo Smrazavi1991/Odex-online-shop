bf4ca39bb77a071db9ebd79c94d684be939a7ac2
SMR <Smrazavi1991@hotmail.com>
2023-06-05 23:47:23 +0330

    Add two path for new APIs in order app urls for calculate discount and total price

--- src/order/api/v1/urls.py
+++ src/order/api/v1/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import AddToCartView, RemoveFromCartView, UpdateCart
+from .views import AddToCartView, RemoveFromCartView, UpdateCart, CalculateTotal, CalculateDiscount
 from user.views import *
 
 urlpatterns = [
     path("add_to_cart/", AddToCartView.as_view(), name="Add to cart"),
-    path("remove_from_cart/", RemoveFromCartView.as_view(), name="Remove from cart"),
+    path("remove-from-cart/", RemoveFromCartView.as_view(), name="Remove from cart"),
     path("update-cart/", UpdateCart.as_view(), name="Update cart"),
+    path("get-total-price/", CalculateTotal.as_view(), name="Calculate total price"),
+    path("get-discount/", CalculateDiscount.as_view(), name="Calculate discount"),
     # path("register/", Register.as_view(), name="Register"),
     # path("verification/", Verification.as_view(), name="Verification"),
     # path("login/", Login.as_view(), name="Login"),
