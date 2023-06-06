d7f8a394a1ce2d9e8eaab35e1b30a3b85efedd33
SMR <Smrazavi1991@hotmail.com>
2023-06-06 22:47:25 +0330

    Change ObtainToken endpoint access level to allow any because just logged in users can send request for obtain a token, change the way that can obtain user from session because the previous way did not work and change the content that set in a JWT token and add an endpoint to deal with user address.

--- src/user/api/v1/views.py
+++ src/user/api/v1/views.py
@@ -1,30 +1,32 @@
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+from django.db.models import Q
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from user.authentication import JWTAuthentication
 import datetime
 
-from .serializers import UserInformationSerializer
-
+from .serializers import UserInformationSerializer, UserAddressSerializer
+from user.models import User, Address
 
 
 class ObtainTokenView(APIView):
+    permission_classes = [AllowAny]
 
-    @method_decorator(login_required)
     def get(self, request):
-        user = self.request.user
+        user = self.request.session.get('username', None)
+        if user:
+            # Generate the JWT token
+            jwt_token = JWTAuthentication.create_jwt(user)
 
-        # Generate the JWT token
-        jwt_token = JWTAuthentication.create_jwt(user)
+            response = Response({'token': jwt_token})
+            expires = datetime.datetime.now() + datetime.timedelta(weeks=999)
+            expires_string = expires.strftime("%a, %d-%b-%Y %H:%M:%S")
+            response.set_cookie("token", jwt_token, expires=expires_string)
 
-        response = Response({'token': jwt_token})
-        expires = datetime.datetime.now() + datetime.timedelta(weeks=999)
-        expires_string = expires.strftime("%a, %d-%b-%Y %H:%M:%S")
-        response.set_cookie("token", jwt_token, expires=expires_string)
-
-        return response
+            return response
+        else:
+            raise AuthenticationFailed('Invalid username')
 
 
 class UserInformation(APIView):
@@ -44,9 +46,29 @@ class UserInformation(APIView):
         return Response(serializer_.data)
 
 
-class UserAddress(APIView):
-    pass
+# class UserAddress(APIView):
+#     pass
 
 
 class ChangePassword(APIView):
     pass
+
+
+class UserAddress(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserAddressSerializer
+
+    def get(self, request):
+        username = self.request.session.get('username', None)
+        user = User.objects.get(username=username)
+        condition1 = Q(user__id=user.pk)
+        condition2 = Q(is_deleted=False)
+        user_address = Address.objects.filter(condition1 & condition2)
+        serializer_ = self.serializer_class(user_address, many=True)
+        return Response(serializer_.data)
+
+    def post(self, request):
+        serializer_ = self.serializer_class(data=request.data)
+        serializer_.is_valid(raise_exception=True)
+        serializer_.save()
+        return Response({'status': 'ok'})
