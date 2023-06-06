1a86d50a00251bc9dbb879fddbfd57f8f2244e59
SMR <Smrazavi1991@hotmail.com>
2023-06-06 22:32:35 +0330

    Add user address model serializer

--- src/user/api/v1/serializers.py
+++ src/user/api/v1/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from user.models import User
+from user.models import User, Address
 
 
 class UserInformationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'phone', 'birthday', 'gender', 'profile_pic', 'is_active']
+
+
+class UserAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = ['pk', 'province', 'city', 'address', 'postal_code']
