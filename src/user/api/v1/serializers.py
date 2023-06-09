82cbb0c2a98468bffd9f0c89017fb6364ce249e1
SMR <Smrazavi1991@hotmail.com>
2023-06-09 23:01:49 +0330

    Add two new field to serialized object of cart models: shipping_method, that show info about order shipping method and cart_item_pics, that must retrieve every product in cart's primary pic(now it's not working). make create_date field values in jalali format and add 'pk' field to fields that a UserOrderSerializer serialize it in every objects of Order Model that received.

--- src/user/api/v1/serializers.py
+++ src/user/api/v1/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from user.models import User, Address
 from order.models import Order, Cart
+from product.models import Product
+from core.views import ProductsViewMixin
 from django_jalali.serializers.serializerfield import JDateField
 
 
@@ -19,16 +21,36 @@ class UserAddressSerializer(serializers.ModelSerializer):
 
 
 class UserCartSerializer(serializers.ModelSerializer):
-    address = UserAddressSerializer()
+    address = UserAddressSerializer(required=True)
+    shipping_method = serializers.SerializerMethodField()
+    cart_item_pics = serializers.SerializerMethodField()
 
     class Meta:
         model = Cart
-        fields = ['customer', 'item', 'shipping_price', 'address', 'total_price']
+        fields = ['customer', 'item', 'shipping_price', 'address', 'total_price', 'shipping_method', 'cart_item_pics']
+
+    def get_shipping_method(self, obj):
+        if obj.shipping_price == "18000 تومان":
+            shipping_method = "ارسال اکسپرس (تحویل در 3 - 5 روز کاری)"
+        else:
+            shipping_method = "ارسال معمولی (تحویل در 5 - 7 روز کاری)"
+        return shipping_method
+
+    def get_cart_item_pics(self, obj):
+        print(type(obj.item))
+        for i in obj.item:
+            product = Product.objects.get(pk=i.pk)
+            if obj.item.index(i) == 0:
+                pic = ProductsViewMixin.get_pics_from_a_product_queryset(product, is_primary=True)
+            else:
+                pic.append(ProductsViewMixin.get_pics_from_a_product_queryset(product, is_primary=True))
+        return pic
 
 
 class UserOrderSerializer(serializers.ModelSerializer):
-    cart_id = UserCartSerializer()
+    cart = UserCartSerializer(required=True)
+    create_date = JDateField(format='%Y-%m-%d')
 
     class Meta:
         model = Order
-        fields = ['create_date', 'cart_id', 'status']
+        fields = ['pk', 'create_date', 'cart', 'status']
