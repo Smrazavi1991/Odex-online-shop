7968c1feff3d681eedcfc2db40593b84163e28a0
SMR <Smrazavi1991@hotmail.com>
2023-06-14 16:09:49 +0330

    Do some Change in paths according to change the API swagger-making tool from drf_yasg to drf_spectacular and do some cleaning according to PEP

--- src/config/urls.py
+++ src/config/urls.py
@@ -17,27 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 from product.views import *
 from user.views import *
 from order.views import *
 from user import urls
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
-)
 
 
 loginpatterns = [
@@ -47,9 +32,11 @@ loginpatterns = [
 ]
 
 urlpatterns = [
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
     path("admin/", admin.site.urls),
     path("", Home.as_view(), name="Home-page"),
     path("register/", Register.as_view(), name="Register"),
@@ -65,4 +52,5 @@ urlpatterns = [
     path("product/<int:pk>/", ProductDetails.as_view(), name="Product-page"),
     path("api/v1/user/", include('user.api.v1.urls')),
     path("api/v1/cart/", include('order.api.v1.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.
+                                                                             MEDIA_ROOT)
